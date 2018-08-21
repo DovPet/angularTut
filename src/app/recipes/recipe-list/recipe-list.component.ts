8dc0c539eb06a9b8c4fab6c250eed7671be21155
DovydasPetrutis <Dovydas.petrutis@gmail.com>
2018-08-21 23:56:13 +0300

    Comunication between components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,16 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     // tslint:disable-next-line:max-line-length
     new Recipe('Test Name', 'Test Description', 'https://s2.15min.lt/static/cache/ODgweDU4MCwweDAsNjE2MzUwLG9yaWdpbmFsLCxpZD01OTY0NjkmZGF0ZT0yMDEyJTJGMDklMkYyNSwzMDU1NzE3ODY3/lazaniija-su-vistiena-50617b8b73c47.jpg'),
     // tslint:disable-next-line:max-line-length
     new Recipe('Test Name', 'Test Description', 'https://s2.15min.lt/static/cache/ODgweDU4MCwweDAsNjE2MzUwLG9yaWdpbmFsLCxpZD01OTY0NjkmZGF0ZT0yMDEyJTJGMDklMkYyNSwzMDU1NzE3ODY3/lazaniija-su-vistiena-50617b8b73c47.jpg')
   ];
+
   constructor() { }
 
   ngOnInit() {
   }
-
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
