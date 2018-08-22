bc581c43e000326dc5c856cc4f1af317b9cda25f
Dovydas Petrutis <Dovydas.Petrutis@atea.lt>
2018-08-22 16:42:57 +0300

    Making details appear near list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,7 +12,7 @@ export class RecipeListComponent implements OnInit {
     // tslint:disable-next-line:max-line-length
     new Recipe('Test Name', 'Test Description', 'https://s2.15min.lt/static/cache/ODgweDU4MCwweDAsNjE2MzUwLG9yaWdpbmFsLCxpZD01OTY0NjkmZGF0ZT0yMDEyJTJGMDklMkYyNSwzMDU1NzE3ODY3/lazaniija-su-vistiena-50617b8b73c47.jpg'),
     // tslint:disable-next-line:max-line-length
-    new Recipe('Test Name', 'Test Description', 'https://s2.15min.lt/static/cache/ODgweDU4MCwweDAsNjE2MzUwLG9yaWdpbmFsLCxpZD01OTY0NjkmZGF0ZT0yMDEyJTJGMDklMkYyNSwzMDU1NzE3ODY3/lazaniija-su-vistiena-50617b8b73c47.jpg')
+    new Recipe('Test Name2', 'Test Description2', 'https://s2.15min.lt/static/cache/ODgweDU4MCwweDAsNjE2MzUwLG9yaWdpbmFsLCxpZD01OTY0NjkmZGF0ZT0yMDEyJTJGMDklMkYyNSwzMDU1NzE3ODY3/lazaniija-su-vistiena-50617b8b73c47.jpg')
   ];
 
   constructor() { }
